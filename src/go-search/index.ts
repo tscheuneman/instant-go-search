a8cd78ef4678255a98c253117c6b47e07137511a
Thomas Scheuneman <thomas@tswebvisions.com>
2022-03-27 17:43:44 -0700

    Add utils and actual http call in our goClient impl

--- src/go-search/index.ts
+++ src/go-search/index.ts
@@ -1,3 +1,6 @@
+
+const ALLOWED_PARAMS = ['limit'];
+
 export class GoSearchClient {
 
     clientUrl: string;
@@ -5,17 +8,69 @@ export class GoSearchClient {
         this.clientUrl = url;
     }
 
-    async search(query: string|undefined, param2: any) {
+    async search(query: string|undefined, searchReqParams: any) {
         console.log('query', query);
-        console.log('pararm2', param2);
+        console.log('pararm2', searchReqParams);
         const searchParams = new URLSearchParams();
+        const filterMapping: Record<string, string[]> = {};
 
         searchParams.append('q', query || '');
 
-        for(const param in param2) {
-            searchParams.append(param, param2[param]);
-            console.log(param);
+        for(const param in searchReqParams) {
+            const paramValue = searchReqParams[param];
+            if(ALLOWED_PARAMS.includes(param)) {
+                searchParams.append(param, paramValue);
+            }
+
+            if(param === 'filter') {
+                if(Array.isArray(paramValue)) {
+                    paramValue.forEach(filterValue => {
+                        if(Array.isArray(filterValue)) {
+                            filterValue.forEach(facetFilter => {
+                                const [key, value] = facetFilter.split('=');
+                                const normalizedVal = this.parseFilterValue(value);
+                                if(filterMapping[key]) {
+                                    filterMapping[key].push(normalizedVal);
+                                } else {
+                                    filterMapping[key] = [normalizedVal]
+                                }
+                            });
+                        }
+                    });
+                }
+                this.constructFacetQueryString(filterMapping, searchParams);
+            }
         }
-        console.log(searchParams.toString());
+
+        const searchUrl = `${this.clientUrl}?${searchParams.toString()}`;
+
+        const searchResult = await fetch(searchUrl, {});
+
+        const resultJson = await searchResult.json();
+        return resultJson;
+    }
+
+    private parseFilterValue = (value: string): string => {
+        let returnString = value;
+        if (returnString.startsWith('"')) {
+            returnString = returnString.substring(1);
+        }
+        if(returnString.endsWith('"')) {
+            returnString = returnString.slice(0, -1)
+        }
+
+        return returnString;
+    }
+
+    private constructFacetQueryString = (facets: Record<string, string[]>, urlSearchParams: URLSearchParams): void => {
+        Object.keys(facets).forEach(key => {
+            const facetsInKey = facets[key];
+            if(Array.isArray(facetsInKey)) {
+                console.log(facetsInKey);
+                urlSearchParams.append(key, facetsInKey.join(','));
+            }
+        });
+
+        urlSearchParams.toString();
     }
 }
